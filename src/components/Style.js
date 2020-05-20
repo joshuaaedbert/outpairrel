15b333c1cf1dd13fa964f8ff3353d939e1b803a8
weha <wilbert26harriman@gmail.com>
2020-05-20 10:32:28 +0800

    Added Home, Discover, Style and Wardrobe

---
+++ src/components/Style.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Style.css'
+
+export default class Style extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="text-danger">STYLE</h1>
+            </div>
+        )
+    }
+}
---
+++ src/components/Wardrobe.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Wardrobe.css'
+
+export default class Wardrobe extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="text-warning">WARDROBE</h1>
+            </div>
+        )
+    }
+}
