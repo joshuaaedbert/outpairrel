9451038efe39518736a4ce9f296161e779e9f2c5
ervinsamuel <ervsml@gmail.com>
2020-06-01 22:38:41 +0800

    added ProductQuery.js

---
+++ src/components/ProductQuery.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ProductQuery extends Component {
+  render() {
+    return (
+      <div>
+        <h2>ProductQuery</h2>
+      </div>
+    )
+  }
+}
