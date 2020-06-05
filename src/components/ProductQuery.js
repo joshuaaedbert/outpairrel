12ee39e62a360545840112da59ac2d3bc64f28d1
ervinsamuel <ervsml@gmail.com>
2020-06-05 17:31:03 +0800

    created ProductQuery.js

--- src/components/ProductQuery.js
+++ src/components/ProductQuery.js
@@ -1,11 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {
+    ListGroup,
+    ListGroupItem
+} from 'reactstrap';
+import ProductItem from './ProductItem';
+import { useSelector } from 'react-redux';
+
 
 export default class ProductQuery extends Component {
+  
+
   render() {
+    const { products } = this.props;
+    let children = (
+      <ListGroupItem className="empty d-flex justify-content-center align-items-center">
+        <div className="empty-text">The End.</div>
+      </ListGroupItem>
+    );
+    if (products.length) {
+      children = products.map((product) => (
+        <ListGroupItem key={product.id}>
+          <ProductItem id={product.id} photo={product.photo} like={product.like} />
+        </ListGroupItem>
+      ));
+    }
     return (
       <div>
-        <h2>ProductQuery</h2>
+        <ListGroup>{children}</ListGroup>
       </div>
-    )
+    );
   }
 }
