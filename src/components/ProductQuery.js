ea6f1ebe8abe6ffc10c39e384b92d9ff02fd4e7d
ervinsamuel <ervsml@gmail.com>
2020-06-11 16:36:44 +0800

    edit ProductQuery

--- src/components/ProductQuery.js
+++ src/components/ProductQuery.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
-import {
-    ListGroup,
-    ListGroupItem
-} from 'reactstrap';
+import React from 'react';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 import ProductItem from './ProductItem';
 import { useSelector } from 'react-redux';
 
-
-export default class ProductQuery extends Component {
-  
-
-  render() {
-    const { products } = this.props;
-    let children = (
-      <ListGroupItem className="empty d-flex justify-content-center align-items-center">
-        <div className="empty-text">The End.</div>
+function ProductQuery() {
+  const products = useSelector((state) => state.products.products);
+  let children = (
+    <ListGroupItem className="empty d-flex justify-content-center align-items-center">
+      <div className="empty-text">The End.</div>
+    </ListGroupItem>
+  );
+  if (products.length) {
+    children = products.map((product) => (
+      <ListGroupItem key={product.id}>
+        <ProductItem
+          id={product.id}
+          photo={product.photo}
+          like={product.like}
+        />
       </ListGroupItem>
-    );
-    if (products.length) {
-      children = products.map((product) => (
-        <ListGroupItem key={product.id}>
-          <ProductItem id={product.id} photo={product.photo} like={product.like} />
-        </ListGroupItem>
-      ));
-    }
-    return (
-      <div>
-        <ListGroup>{children}</ListGroup>
-      </div>
-    );
+    ));
   }
+  return (
+    <div>
+      <ListGroup>{children}</ListGroup>
+    </div>
+  );
 }
+
+export default ProductQuery;
