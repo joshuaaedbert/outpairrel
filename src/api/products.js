4f4487c03417e08a22b7dbab287e7c4802d00bcb
weha <wilbert26harriman@gmail.com>
2020-06-25 16:56:20 +0800

    Succesfully retrieve API data

--- src/api/products.js
+++ src/api/products.js
@@ -1,19 +1,15 @@
-// import axios from 'axios';
-// import React from 'react';
-// const key = '36b99220c9mshc808751dbcee44cp15bb91jsn8b4b792f0418';
-// let HennesMauritz = 'https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/regions/list';
+import axios from "axios";
 
 function getProduct() {
-    let url = "https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/list?categories=men_all&sortBy=stock&concepts=H%2526M%20MAN&country=us&lang=en&currentpage=0&pagesize=30";
+    let url = "https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/list?categories=women_all&sortBy=stock&concepts=H%2526M%20MAN&country=us&lang=en&currentpage=0&pagesize=30";
     console.log(`Making GET request to ${url}`);
-    fetch(url, {
-        "method": "GET",
-        "headers": {
+    return axios.get(url, {
+        headers: {
             "x-rapidapi-host": "apidojo-hm-hennes-mauritz-v1.p.rapidapi.com",
             "x-rapidapi-key": "36b99220c9mshc808751dbcee44cp15bb91jsn8b4b792f0418"
         }
-    }).then(response => {
-        console.log(response);
+    }).then(res => {
+        console.log(res.data.results);
     }).catch(err => {
         console.log(err);
     });
