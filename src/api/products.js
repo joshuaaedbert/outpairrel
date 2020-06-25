03d7b1065e34dd3c817c930e78f3f5a872b16da8
weha <wilbert26harriman@gmail.com>
2020-06-25 16:59:42 +0800

    Successfully retrieved API data

--- src/api/products.js
+++ src/api/products.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 function getProduct() {
-    let url = "https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/list?categories=women_all&sortBy=stock&concepts=H%2526M%20MAN&country=us&lang=en&currentpage=0&pagesize=30";
+    let url = "https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/list?categories=men_all&sortBy=stock&concepts=H%2526M%20MAN&country=us&lang=en&currentpage=0&pagesize=30";
     console.log(`Making GET request to ${url}`);
     return axios.get(url, {
         headers: {
