da6c86f71770ffae165266ccfcf7f5cad6b76793
weha <wilbert26harriman@gmail.com>
2020-06-25 17:40:37 +0800

    Displaying products data

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,20 @@ import './Home.css';
 import getProduct from '../api/products';
 
 function Home() {
+  const [productList, updateProducts] = useState([]);
+
   useEffect(function() {
-    getProduct()
+    getProduct().then(products => {
+      updateProducts(products);
+      console.log(products);
+    })
   }, []);
-  const posts = useSelector(state => state.posts.posts);
+
+  // const posts = useSelector(state => state.posts.posts);
+  
   return (
     <div className="posts">
-      <PostList posts={posts} />
+      <PostList posts={productList} />
     </div>
   );
 }
