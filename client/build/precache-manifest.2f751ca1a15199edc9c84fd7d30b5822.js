5b502cf24fbf87671538401291c7e2c5240f255d
venesia <venesia1808@gmail.com>
2020-06-28 13:59:59 +0800

    VENESIA DUTGEN

---
+++ client/build/precache-manifest.2f751ca1a15199edc9c84fd7d30b5822.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22af18283f461d55deb1ba50c5055edb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dd66d78dfbd46d96f29",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "be00c71b57d2ed4edfdb",
+    "url": "/static/css/main.05519fe9.chunk.css"
+  },
+  {
+    "revision": "5dd66d78dfbd46d96f29",
+    "url": "/static/js/2.d2473d96.chunk.js"
+  },
+  {
+    "revision": "a84eeeab3befcb249344b81afc973182",
+    "url": "/static/js/2.d2473d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be00c71b57d2ed4edfdb",
+    "url": "/static/js/main.03f16ed6.chunk.js"
+  },
+  {
+    "revision": "66537b939d0200d7f469",
+    "url": "/static/js/runtime-main.b1afcff3.js"
+  }
+]);
