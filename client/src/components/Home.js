613a31c863f8aa26e9b49098188e8a75fc0a2e1c
weha <wilbert26harriman@gmail.com>
2020-06-28 14:17:51 +0800

    Server is up

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import PostList from './PostList';
+import getProduct from '../api/products';
 import './Home.css';
 
 function Home() {
+  useEffect(() => {
+    getProduct().then(products => {
+      console.log(products)
+    });
+  })
   const posts = useSelector(state => state.posts.posts);
   
   return (
